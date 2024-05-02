85676f06977dd76d52dc442266ec803e8726f299
pradyumn yadav <yadavkeshu2007@gmail.com>
2024-05-02 22:41:05 +0530

    router added

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,16 +1,26 @@
 import React from 'react';
-// import Home from './pages/Home';
-// import Register from './pages/Register';
+import Home from './pages/Home';
+import Register from './pages/Register';
 import Login from './pages/login'
 import './style.css';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
 
 function App() {
   return (
-    <div>
-      <Login />
-      {/* <Register/> */}
-      {/* <Home/> */}
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/'>
+          <Route index element={<Home />} />
+          <Route path='login' element={<Login/>} />
+          <Route path='register' element={<Register/>} />
+        </Route>
+
+      </Routes>
+    </BrowserRouter>
   );
 }
 
